b5fecc32d729fbc9fa9052eb1b3754c02b092ac8
Lilith Tundrus <echofox123@gmail.com>
2018-09-22 16:47:54 -0400

    added basic index.js

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+// Using ES5 strict mode
+'use strict';
+
+// Node/NPM requires and imports
+import chalk from 'chalk';
+
+function main() {
+console.log(chalk.greenBright('TS-Bank Client Starting...'))
+
+}
+
+main();
