a0a8555787069c212072a67c9dc40f13a4cef227
Lilith Tundrus <echofox123@gmail.com>
2018-09-22 23:09:15 -0400

    Created a login file

---
+++ src/login.ts
@@ -0,0 +1,10 @@
+// Using ES5 strict mode
+'use strict';
+
+// This file contains all of the login/authorization methods for ts-bank
+
+
+// Functions for reading the login file
+
+// The manifest for logins is loosely encrypted because this is simulating a 70's experience
+
