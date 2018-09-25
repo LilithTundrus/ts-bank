2192fb09da37dd2df1fef53a3587afc31cf16c38
Lilith Tundrus <echofox123@gmail.com>
2018-09-24 21:45:07 -0400

    testing the login return data

--- src/index.ts
+++ src/index.ts
@@ -8,9 +8,13 @@ import { login } from './login';
 // Main function, where the user is presented with the initial login screen
 function main() {
     // This is assuming an 80 x 25 terminal
+    process.stdout.write("\u001b[2J\u001b[0;0H");
     console.log(chalk.cyanBright('\n░░░░░░░░░░░░░░░░░░░░░░░░░░░░▒▓ Welcome to TS-Bank ▓▒░░░░░░░░░░░░░░░░░░░░░░░░░░░░\n'));
 
-    login();
+    // Log the user in
+    login().then((result) => {
+        console.log(result)
+    })
     return;
 }
 
