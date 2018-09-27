691e796c1ff843aa8d561e79462b536fdb72d09e
Lilith Tundrus <echofox123@gmail.com>
2018-09-26 20:08:10 -0400

    Adding a file for managing CRUD operations

---
+++ src/lib/StorageController.ts
@@ -0,0 +1,12 @@
+// Using ES5 strict mode
+'use strict';
+
+// Node/NPM requires and imports
+import fs from 'fs';
+
+
+// This script will handle all storage/DB/CRUD needs
+
+// Each time data is changed, make sure that EVERYTHING knows about it
+
+// User data is encrypted!
