e5d760a7eedb0c9e7d45394639497e80330dcf28
Lilith Tundrus <echofox123@gmail.com>
2018-09-28 20:03:00 -0400

    Adding and using a routes file

--- src/server/init.ts
+++ src/server/init.ts
@@ -5,11 +5,21 @@
 import chalk from 'chalk';
 import * as express from 'express';
 import { serverListenPort } from '../config';
+import * as bodyParser from 'body-parser';
+import { route } from './routes';
+
 
 // Init the express server
 const server = express.default();
+
+// Tell the server to use the JSON body parser
+server.use(bodyParser.urlencoded({ extended: true }));
+server.use(bodyParser.json());
+
 // Listen on the configured port for any requests
 server.listen(serverListenPort);
+
+route(server);
 
 // This file will be the 'server' for actually handling banking tasks
 
---
+++ src/server/routes.ts
@@ -0,0 +1,23 @@
+// Using ES5 strict mode
+'use strict';
+
+import { login } from './StorageController';
+
+export function route(app) {
+
+    app.route('/test')
+    .get((req, res) => {
+        res.send('200 OK')
+    })
+    
+
+    app.route('/login')
+        // GET for login, will send a message about login information
+        .post(login);
+
+
+    // app.route('/tasks/:taskId')
+    //     .get(todoList.read_a_task)
+    //     .put(todoList.update_a_task)
+    //     .delete(todoList.delete_a_task);
+};
