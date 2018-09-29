0d7983bf5f34c38e2a905166dd13076c1d60a2f5
Lilith Tundrus <echofox123@gmail.com>
2018-09-28 20:34:37 -0400

    Moved the storageController file

---
+++ src/server/StorageController.ts
@@ -0,0 +1,36 @@
+// Using ES5 strict mode
+'use strict';
+
+// Node/NPM requires and imports
+import fs from 'fs';
+
+
+// This script will handle all storage/DB/CRUD needs
+
+// Each time data is changed, make sure that EVERYTHING knows about it
+
+// User data is encrypted!
+
+export function addClient(clientData) {
+
+}
+
+export function updateClientInfo(newClientData) {
+
+}
+
+export function getClientDataByID(clientID) {
+
+}
+
+export function addClientAccount(accountObj) {
+
+}
+
+export function removeClientAccount(accountID) {
+    
+}
+
+export function login(test) {
+    console.log(test)
+}
